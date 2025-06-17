32da4036e3605c460e9fcb6827fef82e91940fda
abhimanyutr <abhimanyurajan396@gmail.com>
2025-06-17 19:57:04 +0530

    JS

---
+++ 14.0 JavaScript/first.js
@@ -0,0 +1,15 @@
+function test()
+{
+    var a = 27;
+    var b = 81;
+     
+    var temp = a;
+    a = b;
+    b = temp;
+     
+    console.log("Swapped Value of a is " +a);
+    console.log("Swapped value of b is " +b);
+
+}
+
+test();

aa5e6560239c4bdcaf7dacf11e41115145497e72
abhimanyutr <abhimanyurajan396@gmail.com>
2025-06-18 21:25:20 +0530

    slicing

---
+++ 14.0 JavaScript/slice.js
@@ -0,0 +1,15 @@
+// var para1 = "Kochi is known as the financial, commercial and industrial Capital of Kerala. Kochi is the only city in the country to have a water metro system, which has been described as the world's largest electric boat metro transportation infrastructure.";
+
+// var para66 = para1.slice(0,66);
+
+// console.log(para66);
+
+
+var fname = "abhiManyU";
+
+var char1 = fname.slice(0,1);
+var char2 = fname.slice(1,fname.length);
+
+char1 = char1.toUpperCase();
+char2 = char2.toLowerCase();
+console.log("Hello "+ char1 + char2);
